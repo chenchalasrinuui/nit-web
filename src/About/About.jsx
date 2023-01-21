b69144d735a885b17f766a22975672e0f2c74473
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2023-01-21 08:43:35 +0530

    added module css

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,9 +1,9 @@
-import "./About.css";
 import React from "react";
+import styles from './About.module.css'
 
 function template() {
   return (
-    <div className="about">
+    <div className={styles.clr}>
       <h1>About</h1>
     </div>
   );
